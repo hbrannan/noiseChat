d5c37920e31e9a7af510ef5f6300e4b77bda4eba
Hannah_Brannan <hannahbrannan@gmail.com>
2016-08-08 16:57:38 -0700

    Add pbx and gitattr and gitig as well as proj additions

--- NoiseChat/chatScreen/ViewController.swift
+++ NoiseChat/chatScreen/ViewController.swift
@@ -13,13 +13,18 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+            //QUERY THE DATABASE HERE. 
+            //
+        [["createdAt": "2", "user": "Mikey", "mssg":"Cik"], {}, {}]
         
-        
-        //QUERY THE DATABASE HERE. FOR NOW:
-        var dummyData = [];
-        var dummyDatum1 : [String: String] = ["createdAt": "1", "user":"HCB", "mssg":"yo"];
+             //dummy data should be an array that takes dictionaries.
+        var dummyData : [[String: String]]()
+        var dummyDatum1 : [String: String] = ["createdAt": "1", "user":"HCB", "mssg":"yo"]
         var dummyDatum2 : [String: String] = ["createdAt": "2", "user":"MDLC", "mssg":"NOT yolo"];
         var dummyDatum3 : [String: String] = ["createdAt": "3", "user":"HCB", "mssg":"NOT yolo"];
+        
+        dummyData[dummyDatum1, dummyDatum2, dummyDatum3];
+        
         
     }
 
