f7d9c9183fc0f6627866034ca9e6d68bc588e9d0
Hannah_Brannan <hannahbrannan@gmail.com>
2016-08-08 11:59:46 -0700

    Add chatScreeen controller and storyboard

---
+++ NoiseChat/chatScreen/Chat.swift
@@ -0,0 +1,13 @@
+//
+//  Chat.swift
+//  NoiseChat
+//
+//  Created by Hannah Brannan on 8/8/16.
+//  Copyright © 2016 Chivalrous Giants. All rights reserved.
+//
+
+import UIKit
+
+class Chat: UIStoryboard {
+
+}
